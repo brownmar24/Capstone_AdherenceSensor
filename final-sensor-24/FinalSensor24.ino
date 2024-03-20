02aa781c59f72b5f665011a58e94fec182ebd221
ayrjanson <yrjanson24@up.edu>
2024-03-20 10:38:01 -0700

    adjusted file structure and file contents

--- final-sensor-24/FinalSensor24.ino
+++ final-sensor-24/FinalSensor24.ino
@@ -10,7 +10,7 @@ Adapted by Margo B, Kaylee M, Anna Y 4/26/2024
 #include <BTScan.h>              // Part of BluetoothSerial
 #include <BluetoothSerial.h>     // Part of BluetoothSerial
 
-//Libraries
+// General Libraries
 #include <SPI.h>                                           // Communication with Serial Peripheral Interface (SPI) devices (ex. microcontroller and other circuits)
 #include <Wire.h>                                          // I2C communication between chips
 #include <SparkFun_RV8803.h>                               // Read and set time on the RTC
@@ -19,7 +19,6 @@ Adapted by Margo B, Kaylee M, Anna Y 4/26/2024
 #include <SparkFun_MAX1704x_Fuel_Gauge_Arduino_Library.h>  // Used to monitor external battery level
 #include <AES.h>                                           // 128-bit CBC for encryption
 #include <AESLib.h>                                        // AES implementation (128-bit CBC) for low-memory conditions
-//#include "BluetoothSerial.h" // Device can send and receive messages to Bluetooth terminal
 #include <CTR.h>               // Part of the crypto libraries
 #include <Crypto.h>            // Rhys Weatherly's crypto library
 #include "arduino_base64.hpp"  // Turns AES encryption into base64
@@ -46,8 +45,8 @@ Adapted by Margo B, Kaylee M, Anna Y 4/26/2024
 
 // Electronic component variables
 RV8803 rtc;                               // Real Time Clock variable - uses the class RV8803
-Adafruit_LIS3DH lis = Adafruit_LIS3DH();  // LIS3DH Accelerometer variable - UPDATE 10/30/2023: looks used
-SFE_MAX1704X battery;                     // External battery object, used to
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();  // LIS3DH Accelerometer variable
+SFE_MAX1704X battery;                     // External battery object, used to initiate low battery mode
 double battery_percent;                   // Holds the current battery percentage
 int low_battery_threshold = 20;           // Threshold for low battery indication
 
@@ -90,15 +89,14 @@ void setup() {
   pinMode(B_LED, OUTPUT);
   pinMode(BUTTON, INPUT);
   pinMode(EVI, OUTPUT);  // Connecting to the Real-Time Clock
-  Wire.begin();          //I2C addresses begin
-  Serial.begin(115200);  //set baud rate
+  Wire.begin();          // I2C addresses begin
+  Serial.begin(115200);  // Set baud rate
 
   //RTC initialization
   if (rtc.begin() == false)  // If the RTC cannot be found or started
   {
     Serial.println(F("Device not found. Please check wiring. Freezing."));  // Prints to the serial monitor that the RTC cannot be found
-    while (1)
-      ;  // NEED INFORMATION: why are they using an infinite loop?
+    while (1);  // NEED INFORMATION: why are they using an infinite loop?
   }
   Serial.println(F("RTC online!"));  // Prints to the serial monitor that the RTC was found
 
@@ -116,7 +114,7 @@ void setup() {
   Serial.println(F("LIS3DH found!"));  // The LIS3DH accelerometer is found
 
   // Set up lis and init readings
-  lis.setRange(LIS3DH_RANGE_4_G);  // NEED INFORMATION: What does this mean?
+  lis.setRange(LIS3DH_RANGE_4_G);
 
   delay(10);   // Delays by 10 milliseconds
   lis.read();  // Takes a reading from the accelerometer
@@ -129,15 +127,13 @@ void setup() {
   // Check if SD is connected correctly
   if (!SD.begin()) {                         // If the SD card cannot be found, follow this condition
     Serial.println(F("Card Mount Failed"));  // Prints to the serial monitor that the SD card cannot be found
-    // Removed: digitalWrite(LED, HIGH); while (1) {}
-    return;  // Returns without further action
+    return;                                  // Returns without further action
   }
   uint8_t cardType = SD.cardType();  // Saves the type of SD card to an unsigned 8-bit integer
 
   if (cardType == CARD_NONE) {                 // If there is no SD card, follow this condition
     Serial.println(F("No SD card attached"));  // Prints that there is no SD card attached
-    // Removed: digitalWrite(LED, HIGH); while (1) {}
-    return;  // Returns with no further action
+    return;                                    // Returns without further action
   }
 
   Serial.print("SD Card Type: ");      // Prints this statement to the serial monitor
@@ -151,7 +147,7 @@ void setup() {
     Serial.println(F("UNKNOWN"));      // Prints that the card is of an unknown type of the serial monitor
   }
 
-  // Initialize the MAX17043
+  // Initialize the MAX17043 battery object
   if (battery.begin() == false) {
     Serial.println(F("Can't find MAX1704X Battery"));
   }
@@ -192,57 +188,54 @@ void loop() {
 
   /*
    * Condition 1: Bluetooth File Transfer
-   * The user presses the button to send the file from the device to a supported computer or mobile device
+   * The user presses the button to connect the sensor to a Bluetooth compatible device to perform various functions:
+   * - Read data off accelerationdata.txt
+   * - Delete data off accelerationdata.txt
   */
 
   // Check for button press
   if (buttonState == 1) {
-    // Bluetooth Transfer Set-Up
-    SerialBT.begin("Adherence_Sensor");  // Sets the name of the device
-    Serial.println(F("The device can connect to Bluetooth"));
-    setColor(0, 0, 255);  // Set LED color to blue
 
-    // TODO: help command
-    /*
-    delay(5000);
-    if (SerialBT.connected()) {
-      SerialBT.println("Enter one of the following commands:");
-      SerialBT.println("'read' - prints out all data in the accelerometerdata.txt file");
-      SerialBT.println("'delete' - resets the accelerometerdata.txt file");
-      SerialBT.println("'exit' - exits Bluetooth mode (please turn off or restart the device after exiting)");
-    }
-    */
+    // Bluetooth Transfer Set-Up
+    SerialBT.begin("Adherence_Sensor");                         // Sets the name of the device
+    Serial.println(F("The device can connect to Bluetooth"));
+    setColor(0, 0, 255);                                        // Set LED color to blue
 
     while (1) {
       if (SerialBT.available()) {                         // If there is data coming in (from Bluetooth)
         String command = SerialBT.readStringUntil('\n');  // Read string until new line
         command.trim();                                   // Remove leading and trailing whitespace
-
+        
+        // Help Command: prints command information
         if (command == "help") {
           SerialBT.println("Enter one of the following commands:");
           SerialBT.println("'read' - prints out all data in the accelerometerdata.txt file");
           SerialBT.println("'delete' - resets the accelerometerdata.txt file");
-          SerialBT.println("'exit' - exits Bluetooth mode");  // (please turn off or restart the device after exiting)
-        } else if (command == "read") {                       // Protocol for the read command
-
+          SerialBT.println("'exit' - exits Bluetooth mode");
+        } 
+        
+        // Read Command: prints all text data on the accelerationdata.txt file
+        else if (command == "read") {
           file = SD.open("/accelerationdata.txt", FILE_READ);  // Opens the file to print to the Bluetooth terminal
-          if (file) {
-            if (file.peek() < 0) {  // file is empty
-              SerialBT.println("File is currently empty.");
-            } else { // file is not empty            
-              //SerialBT.println();
+          if (file) {                                          // Checks if accelerationdata.txt is readable
+            if (file.peek() < 0) {                             // Checks if accelerationdata.txt is empty
+              SerialBT.println("File is currently empty.");    // Prints warning that the file is empty
+            } else {                                           // If accelerationdata.txt is not empty            
               SerialBT.println("Data Below: ");  // Prints all data from the file to the serial monitor
               while (file.available()) {         // Prints the data from the file line-by-line
                 SerialBT.write(file.read());     // Writes the line to the Bluetooth terminal
               }
-              
             }
             file.close();  // Closes the accelerometerdata.txt file
-          } else {  // If the file cannot be opened
+          } 
+          else {  // If the file cannot be opened
             Serial.println(F("ERROR: Cannot open file"));
             SerialBT.println("ERROR: Cannot open /accelerationdata.txt; File Size: " + String(SD.open("/accelerationdata.txt").size()));
           }
-        } else if (command == "delete") {            // Protocol for the delete command
+        } 
+        
+        // Delete Command: deletes existing data on the accelerationdata.txt file
+        else if (command == "delete") {
           SD.remove("/accelerationdata.txt");        // Deletes the accelerationdata.txt file from the SD card
           if (SD.exists("/accelerationdata.txt")) {  // Checks that the file stil exists
             Serial.println(F("Delete functionality did not work"));
@@ -254,36 +247,54 @@ void loop() {
         } else if (command == "exit") {                  // Protocol for the exit command
           SerialBT.println("Bluetooth disconnecting.");  // Prompts the user to reset the device to normal collection
           delay(5000);
-
-          SerialBT.end();
-          break;  // Exits to the main loop() function
-        } else {  // Asks the user to input a valid command
+          SerialBT.end();                                // Disconnects the bluetooth from the serial monitor
+          break;                                         // Exits to the main loop() function
+        } 
+        
+        // Invalid Command: prompts the user for a valid command
+        else {  
           SerialBT.println("Invalid command: " + command);
         }
       }
     }
     // If the user (incorrectly) leaves the device on after exiting, the following will execute before exiting this condition
-    //delay(5000);          // Five-second delay
     buttonState = 0;      // Returns the button variable to the "not-pushed" state
     setColor(0, 255, 0);  // Sets the LED color back to green
   }
 
-  /* CHECK FOR IDLE MODE HERE */
+  /*
+   * Condition 2: Idle Mode Check
+   * Puts the accelerometer into low power mode if the cart has been idle for longer than 30 seconds
+   * Conducts regular motion detection in low power mode
+  */
+
   else if (current_time - stop_motion > 30000) {
     setColor(255, 30, 0);                            // Sets the color to "orange-ish"
     lis.setDataRate(LIS3DH_DATARATE_LOWPOWER_5KHZ);  // Sets the accelerometer to low-power mode
     motionDetection();                               // Resumes motion detection
   }
 
+  /*
+   * Condition 3: Low Battery Check
+   * Checks if the battery level is below 20% and displays a red LED
+   * Conducts regular motion detection in full power mode
+  */
+
   // Checks if battery is below 20 percent
   else if (battery_percent < 20) {
     setColor(255, 0, 0);  // Sets the LED color to red
     motionDetection();    // Runs the motion detection
+
     // Print the battery percentage
     Serial.print("Battery Percentage: ");
     Serial.print(battery_percent);
     Serial.println("%");
   }
+
+  /*
+   * Condition 4: Regular Motion Detection
+   * Sets the LED color to green and conducts regular motion detection
+  */
 
   else {
     setColor(0, 255, 0);                     // Sets the LED color to green
@@ -291,6 +302,11 @@ void loop() {
     motionDetection();                       // Runs the motion detection
   }
 }
+
+/*
+ * void motionDetection
+ * Reads accelerometer data and uses a threshold of change to determine if the child is moving
+*/
 
 // Accelerometer: Uses a delta scheme to detect movement
 void motionDetection() {
@@ -338,7 +354,11 @@ void motionDetection() {
   delay(100);  // 100 milliseconds between readings
 }
 
-//timestamp subroutine
+/*
+ * String RTC
+ * Creates a Real-Time Clock timestamp for recording the current time
+*/
+
 String RTC() {
   String time;  // The string used to save the timestamp data
   //RTC timestamp
@@ -360,6 +380,11 @@ String RTC() {
   }
   return time;  // Returns the timestamp string
 }
+
+/*
+ * void accelRead
+ * Takes 50 accelerometer readings and averages them
+*/
 
 //read from the accelerometer function puts data through moving average filter
 void accelRead() {
@@ -400,6 +425,11 @@ void sd_Start() {  //prints to sd card starting
   encryptAndWrite(start_encrypt);
 }
 
+/*
+ * void sd_Stop
+ * Prints the time of stopped motion to accelerationdata.txt
+*/
+
 void sd_Stop() {  //printing to the SD card
 
   String stopTime = RTC();  //timestamp
@@ -407,9 +437,13 @@ void sd_Stop() {  //printing to the SD card
 
   // The acclerationdata.txt file could be read
   String stop_encrypt = "Time of Stop Detected At: " + stopTime + "\n";
-  encryptAndWrite(stop_encrypt);
-  //file.print(stop_encrypt); // Appends this to the bottom of the file when motion is stopped
+  encryptAndWrite(stop_encrypt); // Appends this to the bottom of the file when motion is stopped
 }
+
+/*
+ * void encryptAndWrite
+ * Encrypts data and writes to the accelerationdata.txt file
+*/
 
 void encryptAndWrite(String data) {
   file = SD.open("/accelerationdata.txt", FILE_APPEND);  //open file.txt to write data
@@ -424,12 +458,12 @@ void encryptAndWrite(String data) {
     file.println(data);     // Prints encrypted data to the file
     Serial.println(data);   // Prints the encurpted file to the serial terminal
     Serial.println(data2);  // Prints the decrypted data to the serial terminal
-    // Serial.println(data2);
     file.close();  // Closes accelerationdata.txt file
   }
 }
 
 // The encryption function
+// Source: https://adriancs.com/c-sharp/1081/arduino-aes-encrypt-csharp-decrypt/
 String encrypt(String inputText) {
 
   // calculate the length of bytes of the input text
@@ -483,6 +517,7 @@ String encrypt(String inputText) {
 }
 
 // The decryption function
+// Source: https://adriancs.com/c-sharp/1081/arduino-aes-encrypt-csharp-decrypt/
 String decrypt(String encryptedBase64Text) {
 
   // calculate the original length before it was coded into base64 string
@@ -525,7 +560,11 @@ String decrypt(String encryptedBase64Text) {
   return decryptedText;
 }
 
-// Sets the color of the LED
+/*
+* void setColor
+* Sets the color of the RGB LED
+*/
+
 void setColor(int redValue, int greenValue, int blueValue) {
   analogWrite(R_LED, redValue);
   analogWrite(G_LED, greenValue);
